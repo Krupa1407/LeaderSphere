b128db0a4cee9ffd35215c682696818df28ce44d
Krupa1407 <shobhashnakrupa@gmail.com>
2024-06-17 02:00:07 +0530

    update functionality of books and awards done

--- frontend/src/components/UpdateLeader.js
+++ frontend/src/components/UpdateLeader.js
@@ -27,13 +27,16 @@ const Updateleader = () => {
       setCategory(result.category);
       setDesc(result.desc);
       setAge(result.age);
+      setBooks(result.books);
+      setAwards(result.awards);
+      console.log(result.books);
     }
 
     const updateLeader = async (req, res) => {
         console.log(name, category, desc, age, books, awards);
         let result = await fetch(`http://localhost:5000/leader/${params.id}`,{
           method: "Put",
-          body: JSON.stringify({name, category, desc, age}),
+          body: JSON.stringify({name, category, desc, age, books,awards}),
           headers: {
             "Content-Type" : "application/json"
           }
@@ -43,25 +46,50 @@ const Updateleader = () => {
         console.log(result);
     }
 
+    const handleBookChange = (index, value) => {
+      const newBooks = [...books];
+      newBooks[index] = value;
+      setBooks(newBooks);
+    }
+
+    const handleAwardChange = (index, value) => {
+      const newAwards = [...awards];
+      newAwards[index] = value;
+      setAwards(newAwards);
+    }
+
+
   return (
-    <div className='register'>
+    <div className='register container-add-leader'>
     <h1>Update Leader</h1>
     <input className="inputBox" type="text" placeholder='enter name' value={name}  onChange={(e) => setName(e.target.value)}/>
     <input className="inputBox"  type="text" placeholder='enter category' value={category} onChange={(e) => setCategory(e.target.value)}/>
     <input className="inputBox"  type="text" placeholder='enter desc' value={desc} onChange={(e) => setDesc(e.target.value)}/>
     <input className="inputBox"  type="text" placeholder='enter age' value={age} onChange={(e) => setAge(e.target.value)}/>
-    {/* <ul className= "card-text">
-        <li style={{ listStyle: 'none', fontWeight: 'bold' }}>Books:</li>
-        {item.books.map((book, index) => (
-          <input type="text" value={book} key={index}/>
+    <ul className= "card-text">
+        <p style={{ fontWeight: 'bold', paddingLeft: '2vh' }}>Books:</p>
+        {books.map((book, index) => (
+            <input
+                className="inputBox"
+                type="text"
+                value={book}
+                key={index}
+                onChange={(e) => handleBookChange(index, e.target.value)}
+            />
         ))}
     </ul>
     <ul className= "card-text">
-        <li style={{ listStyle: 'none', fontWeight: 'bold' }}>Awards:</li>
-        {item.awards.map((award, index) => (
-            <li style={{ listStyle: 'none' }} key={index}>{award}</li>
+        <p style={{ fontWeight: 'bold', paddingLeft: '2vh' }}>Awards:</p>
+        {awards.map((award, index) => (
+            <input
+                className="inputBox"
+                type="text"
+                value={award}
+                key={index}
+                onChange={(e) => handleAwardChange(index, e.target.value)}
+            />
         ))}
-    </ul> */}
+    </ul>
     <button onClick={updateLeader} className="inputBox regbtn" type="button">Update Leader</button>
      </div>
   )
