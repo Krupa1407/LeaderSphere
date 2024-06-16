adf1e8ac97fc9f6933957c0f9ba3c4daf6a28641
Krupa1407 <shobhashnakrupa@gmail.com>
2024-06-16 14:45:58 +0530

    login in the backend(check if signed up or not?) return aknowledgement as the user

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,32 @@ app.use(cors());
 
 app.use(express.json());
 app.post("/register", async(req,res) => {
-    res.send(req.body);
+    // res.send(req.body);
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    res.send(result);
 });
 
 
-
+app.post("/login", async(req, res) => {
+    // res.send(req.body);      //for checkin if the route is working properly.
+    if(req.body.email && req.body.password){
+        let user = await User.findOne(req.body).select("-password");
+        if(user){
+            res.send(user);
+        }
+        else{
+            user = "no result found."
+            res.send(user);
+        }
+    }
+    else{
+            let user = "no result found."
+            res.send(user);
+    }
+});
 
 
 
